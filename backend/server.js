c6a93750bb8bb4661628f3290431e9259312f38a
rjratefy <jessicamialy2@gmail.com>
2026-02-05 08:31:04 +0300

    connexion Base

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
+require("dotenv").config();
+
 const express = require('express');
+const cors = require('cors');
+const connectDB = require("./config/db");
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware pour parser le JSON
+// Connexion MongoDB Atlas
+connectDB();
+
+// Middleware
 app.use(express.json());
+app.use(cors());
 
-// Route de test
+// Route test
 app.get('/', (req, res) => {
   res.send('Backend MEAN en marche 🚀');
 });
 
-// Lancer le serveur
+// Lancer serveur
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
