00f844d05953a4bc7f4ddfbb3adf1a90592ff559
Maela Andriamahery <andriamaela@gmail.com>
2026-02-17 10:46:23 +0300

    update server.js add index.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
 
-// Middleware pour parser le JSON
-app.use(express.json());
+// // Middleware pour parser le JSON
+// app.use(express.json());
 
-// Route de test
-app.get('/', (req, res) => {
-  res.send('Backend MEAN en marche 🚀');
-});
+// // Route de test
+// app.get('/', (req, res) => {
+//   res.send('Backend MEAN en marche 🚀');
+// });
 
-// Lancer le serveur
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
-});
+// // Lancer le serveur
+// app.listen(PORT, () => {
+//   console.log(`Serveur démarré sur http://localhost:${PORT}`);
+// });
+
+var http = require("http");
+
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+  http.creqteServert(onRequest).listen(8888);
+  console.log("Démarrage du serveur.");
+}
+
+exports.start = start;
